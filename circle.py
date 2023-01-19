96f3dda870ef4df650c3a0d6de205a970ea65230
meenu336 <120801029+meenu336@users.noreply.github.com>
2023-01-20 01:43:26 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,8 @@
+#python program to find the radius and perimeter of circle
+radius=int(input("enter the radius of the circle:"))
+#calculate the radius of circle
+area=3.14*radius*radius
+print("area of circle",area)
+#calculate the perimeter of circle
+perimeter=2*3.14*radius
+print("perimeter of circle",perimeter)
